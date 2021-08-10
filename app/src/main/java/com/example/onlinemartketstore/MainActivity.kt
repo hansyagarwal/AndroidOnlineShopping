321c607138f99e23468eb386891006320057c306
Aryan Agarwal <hansy.agarwal@gmail.com>
2021-08-11 01:05:15 +0530

    signup/signin UI

--- app/src/main/java/com/example/onlinemartketstore/MainActivity.kt
+++ app/src/main/java/com/example/onlinemartketstore/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.onlinemartketstore
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +17,15 @@ class MainActivity : AppCompatActivity() {
 
         signInB = findViewById(R.id.alreadyAcc)
         signUpB = findViewById(R.id.createAcc)
+    }
+
+    fun signIn(view: View) {
+        val i = Intent(this, LoginActivity::class.java)
+        startActivity(i)
+    }
+
+    fun signUp(view: View) {
+        val i = Intent(this,SignUpActivity::class.java)
+        startActivity(i)
     }
 }
